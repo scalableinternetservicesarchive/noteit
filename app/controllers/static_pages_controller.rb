12e116319ebdba90bb9409073bb1732ffd683684
Aviral Takkar <aviraltakkar@gmail.com>
2015-10-16 18:04:32 -0700

    cleaning up layout, all routes set

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,10 @@
 class StaticPagesController < ApplicationController
+  before_action :authenticate_user!, except: [:welcome_page]
   def home
   end
   def welcome_page
+  	if user_signed_in?
+  		redirect_to static_pages_home_path
+  	end
   end
 end
