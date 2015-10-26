613c80636bacaeb5971568da3ab3d63a674ad668
otownsend92 <otownsend92@gmail.com>
2015-10-25 22:09:26 -0700

    Redoing homepage

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,6 +15,11 @@ Rails.application.config.assets.precompile += %w( bootstrap.min.css )
 Rails.application.config.assets.precompile += %w( bootstrap-tags.css )
 Rails.application.config.assets.precompile += %w( vote.css )
 Rails.application.config.assets.precompile += %w( signup.css )
+Rails.application.config.assets.precompile += %w( simple-sidebar.css )
+Rails.application.config.assets.precompile += %w( metisMenu.min.css )
+Rails.application.config.assets.precompile += %w( sb-admin.css )
+Rails.application.config.assets.precompile += %w( sb-admin-rtl.css )
+Rails.application.config.assets.precompile += %w( font-awesome.min.css )
 
 
 Rails.application.config.assets.precompile += %w( textAngular-rangy.min.js )
@@ -23,3 +28,10 @@ Rails.application.config.assets.precompile += %w( textAngular.min.js )
 Rails.application.config.assets.precompile += %w( homeController.js )
 Rails.application.config.assets.precompile += %w( bootstrap-tags.js )
 Rails.application.config.assets.precompile += %w( vote.js )
+Rails.application.config.assets.precompile += %w( dropdown.js )
+Rails.application.config.assets.precompile += %w( jquery.min.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( metisMenu.min.js )
+Rails.application.config.assets.precompile += %w( sb-admin-2.js )
+
+
