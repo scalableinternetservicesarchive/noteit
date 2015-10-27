b350de394164b95ceee8e5dea3265f6fc084f198
otownsend92 <otownsend92@gmail.com>
2015-10-26 20:59:38 -0700

    homepage UI with dynamic notebook generation

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,14 @@
 class StaticPagesController < ApplicationController
   before_action :authenticate_user!, except: [:welcome_page]
+
   def home
+    @notes = current_user.notes.paginate(page: params[:page])
   end
+
   def welcome_page
   	if user_signed_in?
   		redirect_to static_pages_home_path
   	end
   end
+
 end
