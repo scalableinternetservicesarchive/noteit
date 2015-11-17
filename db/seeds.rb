844ae69ff35bf95d6dd5015222a982ef17702aa9
Aviral Takkar <aviraltakkar@gmail.com>
2015-11-16 19:15:16 -0800

    add large seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
 User.create!(name:  "Note It User",
              email: "niu@gmail.com",
              password: "12345678",
              )
 
-999.times do |n|
+10000.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"	  
@@ -20,15 +14,25 @@ User.create!(name:  "Note It User",
                )
 end
 user = User.find_by(name: 'Note It User')
-
+r = Random.new
 50.times do
   title = Faker::Lorem.sentence(1)
   user.notebooks.create!(title: title) 
 end
 
 1000.times do
+  user = User.find(r.rand(10000))  
+  50.times do 
+    title = Faker::Lorem.sentence(1)
+    user.notebooks.create!(title: title) 
+  end
+end
+
+user = User.find_by(name: 'Note It User')
+1000.times do
   content = Faker::Lorem.sentence(5)
   title = Faker::Lorem.sentence(1)
   notebook = Notebook.find_by(id: 1)
   user.notes.create!(title: title, content: content, notebook_id: notebook.id) 
 end
+
