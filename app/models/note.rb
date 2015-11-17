39cb71182f4ef22550c10812cc147130e514fad2
Aviral Takkar <aviraltakkar@gmail.com>
2015-11-16 19:41:26 -0800

    fix

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,5 @@
 class Note < ActiveRecord::Base
-	validates :title, presence: true, length: { maximum: 60 }
+	validates :title, presence: true
 	validates :content, presence: true
 	belongs_to :user
 	belongs_to :notebook
