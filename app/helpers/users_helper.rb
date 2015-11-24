2101ebf5c6b9f65ca0d4565341f70b7b090b5cd7
Aviral Takkar <aviraltakkar@gmail.com>
2015-11-24 13:08:50 -0800

    add caching...work in progress

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+def cache_key_for_feed(feed_items)
+   "feed/#{feed_items.count}/#{feed_items.maximum(:updated_at)}"
+end	
+
+def cache_key_for_feed_item(item)
+	"note/#{item.id}/#{item.updated_at}"
+end
+
 end
