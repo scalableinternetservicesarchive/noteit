84a2bf31c261b84860ac39a4919a832c0d3fa637
Aviral Takkar <aviraltakkar@gmail.com>
2015-12-02 16:29:57 -0800

    improve UI, add features, improve seeds

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -16,4 +16,22 @@ def cache_key_for_user(user)
   	"user/#{user.id}/#{user.updated_at}"
 end
 
+def count_upvotes(user)
+	notes =Note.where(user_id = user)
+	count = 0
+	user.notes.each do |note|
+		count += note.votes_for.up.by_type(User).voters.count
+	end
+	return count
+end
+
+def count_downvotes(user)
+	notes =Note.where(user_id = user)
+	count = 0
+	user.notes.each do |note|
+		count += note.votes_for.down.by_type(User).voters.count
+	end
+	return count
+end
+
 end
